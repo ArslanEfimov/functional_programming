0b9b577bcba0692e88cec1da75eba986d0528e63
arslanefimov <dns230717rus@gmail.com>
2024-10-05 15:24:59 +0300

    did formatting

--- lab1_fp/src/euler_22/euler22_tail_recursion.erl
+++ lab1_fp/src/euler_22/euler22_tail_recursion.erl
@@ -2,37 +2,35 @@
 -export([find_final_score/0]).
 
 find_final_score() ->
-  {ok, Binary} = file:read_file("src/euler_22/names.txt"),
-  Names = format_names(binary_to_list(Binary)),
-  SortedNames = quicksort(Names),
-  Scores = calculate_scores(SortedNames),
-  TotalScore = lists:sum(Scores),
-  TotalScore.
+    {ok, Binary} = file:read_file("src/euler_22/names.txt"),
+    Names = format_names(binary_to_list(Binary)),
+    SortedNames = quicksort(Names),
+    Scores = calculate_scores(SortedNames),
+    TotalScore = lists:sum(Scores),
+    TotalScore.
 
-quicksort([]) -> [];
+quicksort([]) ->
+    [];
 quicksort([Pivot | Rest]) ->
-  Less = [X || X <- Rest, X =< Pivot],
-  Greater = [X || X <- Rest, X > Pivot],
-  quicksort(Less) ++ [Pivot] ++ quicksort(Greater).
-
+    Less = [X || X <- Rest, X =< Pivot],
+    Greater = [X || X <- Rest, X > Pivot],
+    quicksort(Less) ++ [Pivot] ++ quicksort(Greater).
 
 format_names(Content) ->
-  NameList = string:tokens(Content, "\",\""),
-  NameList.
+    NameList = string:tokens(Content, "\",\""),
+    NameList.
 
 calculate_scores(Names) ->
-  calculate_scores(Names, 1, []).
+    calculate_scores(Names, 1, []).
 
 calculate_scores([], _, Acc) ->
-  lists:reverse(Acc);
-
+    lists:reverse(Acc);
 calculate_scores([Name | Rest], Index, Acc) ->
-  Score = name_value(Name) * Index,
-  calculate_scores(Rest, Index + 1, [Score | Acc]).
-
+    Score = name_value(Name) * Index,
+    calculate_scores(Rest, Index + 1, [Score | Acc]).
 
 name_value(Name) ->
-  lists:sum([char_value(Char) || Char <- Name, Char >= $A, Char =< $Z]).
+    lists:sum([char_value(Char) || Char <- Name, Char >= $A, Char =< $Z]).
 
 char_value(Char) ->
-  Char - $A + 1.
+    Char - $A + 1.
